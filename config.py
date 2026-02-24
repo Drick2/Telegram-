6896f8dd0f89122acf1f49b965f8f6b3e7857919
Drick2 <lilfameican@gmail.com>
2026-02-24 12:02:50 +0300

    Update group and channel links in config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = "YOUR_TOKEN_HERE"  # ← from BotFather
-GROUP_LINK = "t.me/yourgroup"
-CHANNEL_LINK = "t.me/MATOAirdrop"
+GROUP_LINK = "https://t.me/+2HSGm2-p-dwxNjhk"
+CHANNEL_LINK = "https://t.me/matoairdrop"
 PUMP_FUN_LINK = "https://pump.fun/YOUR_TOKEN_CA_HERE"  # ← after token
 GROUP_ID = -1001234567890  # ← replace with real group ID
